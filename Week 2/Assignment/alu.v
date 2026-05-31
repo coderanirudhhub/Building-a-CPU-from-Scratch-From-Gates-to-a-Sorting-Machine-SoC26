93d2e0d1f521ba92feaaa19ffe2508878b1b8361
coderanirudhhub <nagaanirudhgch@gmail.com>
2026-05-31 23:50:49 +0530

    Implement ALU operations and zero flag logic
    Add zero flag assignment and implement arithmetic operations.

--- Week 2/Assignment/alu.v
+++ Week 2/Assignment/alu.v
@@ -10,4 +10,30 @@ module alu(
     output reg       carry,
     output reg       overflow
 );
+      assign zero = (result == 8'b0000_0000);
+
+    always @(*) begin
+        carry    = 1'b0;
+      overflow = 1'b0;
+
+    if (op == 3'b000) begin
+            {carry, result} = a + b;
+            overflow = (a[7] == b[7]) && (result[7] != a[7]);
+        end 
+    else if(op == 3'b001) begin
+            result = a - b;
+            carry  = (a < b);
+            overflow = (a[7] != b[7]) && (result[7] != a[7]);
+        end 
+      else if(op == 3'b010)
+    result = a & b; 
+    else if(op == 3'b011)
+       result = a | b;
+      else if (op == 3'b100)
+     result = a ^ b;
+    else if (op == 3'b101)
+     result = a << 1;
+    else if (op == 3'b110)
+     result = a >> 1;
+    end
 endmodule
